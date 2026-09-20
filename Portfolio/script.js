b305a3f09eca81ed206746e27a063de2e331e5b7
Lokial <131384848+Lokial@users.noreply.github.com>
2026-09-20 20:21:27 +0800

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,49 @@
+// Simple scroll animation
+
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+
+        });
+    },
+    {
+        threshold: 0.1
+    }
+);
+
+sections.forEach(section => {
+    observer.observe(section);
+});
+
+
+// Certificate image click
+
+const certificateImages = document.querySelectorAll(".certificate img");
+
+certificateImages.forEach(image => {
+
+    image.addEventListener("click", () => {
+
+        window.open(image.src, "_blank");
+
+    });
+
+});
+
+const diplomaImages = document.querySelectorAll(".project-grid img");
+
+diplomaImages.forEach(image => {
+
+    image.addEventListener("click", () => {
+
+        window.open(image.src, "_blank");
+
+    });
+
+});
